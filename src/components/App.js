d7388faaf877d4f3831c3593660a16552b09222d
turhaw <turhaw@gmail.com>
2021-05-27 15:06:16 +0400

    initial

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Button from '@material-ui/core/Button';
+import './App.css';
+import Navbar from './navbar/index'
+
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
